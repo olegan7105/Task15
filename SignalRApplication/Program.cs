9303f497059928b950d4186c1ca97634336c90d6
Пользователь <@DESKTOP-KVSP5S9>
2022-10-13 10:05:02 +0300

    Добавьте файлы проекта.

---
+++ SignalRApplication/Program.cs
@@ -0,0 +1,14 @@
+using SignalRApplication;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSignalR();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+app.MapHub<ChatHub>("/chat");
+
+app.Run();
