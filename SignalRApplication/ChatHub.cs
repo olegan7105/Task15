ed1f47e725b1b09d3a8176b0e4d18476938cf15f
olegan7105 <bubencrutl@mail.ru>
2022-10-13 11:18:14 +0300

    lll

--- SignalRApplication/ChatHub.cs
+++ SignalRApplication/ChatHub.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRApplication
 {
+    [Authorize]
     public class ChatHub : Hub
     {
         public async Task Send(string message, string userName)
         {
             await Clients.All.SendAsync("Receive", message, userName);
+        }
+        [Authorize(Roles = "Admin")]
+        public async Task Notify(string message)
+        {
+            await Clients.All.SendAsync("Receive", message, "Administrator");
         }
     }
 }
--- SignalRApplication/Program.cs
+++ SignalRApplication/Program.cs
@@ -1,14 +1,78 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using SignalRApplication;
+using System.Data;
+using System.Security.Claims;
+
+var adminRole = new Role("admin");
+var userRole = new Role("user");
+var people = new List<Person>()
+{
+    new Person("admin@gmail.com", "12345", adminRole),
+    new Person("user@gmail.com", "54321", userRole)
+};
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-app.UseDefaultFiles();
-app.UseStaticFiles();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGet("/login", async context =>
+    await SendHtmlAsync(context, "html/login.html"));
+
+app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+{
+    var form = context.Request.Form;
+
+    if (!form.ContainsKey("email") || !form.ContainsKey("password"))
+        return Results.BadRequest("Email и/или пароль не установлены");
+    string email = form["email"];
+    string password = form["password"];
+
+    Person? person = people.FirstOrDefault(p => p.Email == email && p.Password == password);
+
+    if (person is null) return Results.Unauthorized();
+
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimsIdentity.DefaultNameClaimType, person.Email),
+        new Claim(ClaimsIdentity.DefaultRoleClaimType, person.Role.Name)
+    };
+    var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+    await context.SignInAsync(claimsPrincipal);
+    return Results.Redirect(returnUrl ?? "/");
+});
+
+app.MapGet("/", [Authorize] async (HttpContext context) =>
+    await SendHtmlAsync(context, "html/index.html"));
+
+app.MapGet("/admin", [Authorize(Roles = "admin")] async (HttpContext context) =>
+    await SendHtmlAsync(context, "html/admin.html"));
+
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/login");
+});
 
 app.MapHub<ChatHub>("/chat");
-
 app.Run();
+
+async Task SendHtmlAsync(HttpContext context, string path)
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    await context.Response.SendFileAsync(path);
+}
+
+record class Person(string Email, string Password, Role Role);
+record class Role(string Name);
